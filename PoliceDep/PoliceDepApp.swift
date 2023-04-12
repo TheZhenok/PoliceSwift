2251ae5361421eb49bfcbd9554147dc55d33dc53
thezhenok <zenjaprihan@yandex.ru>
2023-04-12 13:58:52 +0600

    Initial Commit

---
+++ PoliceDep/PoliceDepApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoliceDepApp.swift
+//  PoliceDep
+//
+//  Created by ITSTEP on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PoliceDepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
